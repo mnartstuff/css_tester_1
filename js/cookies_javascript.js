59e833e090a6be188bc5bd3bfdc6172cd15c004e
mnartstuff <68624179+mnartstuff@users.noreply.github.com>
2020-09-06 22:44:44 -0500

    Add files via upload

---
+++ js/cookies_javascript.js
@@ -0,0 +1,18 @@
+
+function setDate(){
+  document.getElementById('date').innerHTML=Date();
+}
+
+function cannedham(){
+	document.getElementById('inner').innerHTML="Canned Ham!";
+}
+
+function openMe(){
+	x=document.getElementById('pp');
+	x.style.display="inline-block";
+}
+
+function closeMe(){
+	x=document.getElementById('pp');
+	x.style.display="none";
+}
